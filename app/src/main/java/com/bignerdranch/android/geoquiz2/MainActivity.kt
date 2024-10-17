ad0c6fb96f621bbdfc3610d6cbd329cd22b85fcb
KossiviEsse1 <kossivi14@gmail.com>
2024-10-17 11:53:40 -0500

    Finished Chapter 2 BNR

--- app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
@@ -5,27 +5,69 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.bignerdranch.android.geoquiz2.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    private lateinit var binding: ActivityMainBinding
+
+    private val questionBank = listOf<Question>(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-
-        trueButton.setOnClickListener { view: View ->
-            Snackbar.make(view, R.string.correct_toast, BaseTransientBottomBar.LENGTH_SHORT).show()
+        binding.trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
         }
 
-        falseButton.setOnClickListener { view: View ->
-            Snackbar.make(view, R.string.incorrect_toast, BaseTransientBottomBar.LENGTH_SHORT).show()
+        binding.falseButton.setOnClickListener { view: View ->
+            checkAnswer(false)
+        }
+
+        updateQuestion()
+
+        binding.nextButton.setOnClickListener { view: View ->
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        binding.prevButton.setOnClickListener { view: View ->
+            if (currentIndex - 1 >= 0) {
+                currentIndex = currentIndex - 1
+            } else {
+                currentIndex = questionBank.size - 1
+            }
+            updateQuestion()
+        }
+
+        binding.questionTextView.setOnClickListener { view: View ->
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        if (userAnswer == questionBank[currentIndex].answer) {
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
         }
     }
 }
